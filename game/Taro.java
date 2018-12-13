9fe7af3ad7070cbda977b96c799dcfda1ffc48d4
shingo1998 <shingo_1998@ezweb.ne.jp>
2018-12-13 10:12:15 +0900

    Cがキャラ移動処理を追加

--- game/Taro.java
+++ game/Taro.java
@@ -15,5 +15,9 @@ public class Taro extends Actor
     public void act() 
     {
         // Add your action code here.
+        if( Greenfoot.isKeyDown( "down" ) ){
+        setRotation(90);
+        move(1);
+}
     }    
 }
